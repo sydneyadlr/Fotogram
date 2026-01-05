bf99694dec403a36ff458a5894ba57cdc5fb2075
Sydney Adler <sydneyadler05@gmail.com>
2026-01-05 17:47:16 +0100

    basic conzept html css js

---
+++ script.js
@@ -0,0 +1,15 @@
+let img = [
+    `./img/Component 10.png`,
+    `./img/Component 10.png`,
+    `./img/Component 10.png`,
+    `./img/Component 10.png`,
+    `./img/Component 10.png`,
+    `./img/Component 10.png`,
+    `./img/Component 10.png`,
+    `./img/Component 10.png`,
+    `./img/Component 10.png`,
+    `./img/Component 10.png`,
+    `./img/Component 10.png`,
+    `./img/Component 10.png`,
+]
+
